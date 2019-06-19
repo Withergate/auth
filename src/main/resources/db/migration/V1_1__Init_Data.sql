e0f34291a2a7c8c78e41776b254f393b4dbfcb5a
Martin Myslik <martin.myslik@ataccama.com>
2019-06-19 15:14:08 +0200

    task/#14 Add default user to migrations

---
+++ src/main/resources/db/migration/V1_1__Init_Data.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (user_id, email, pending_email, password, role, enabled) VALUES
+	('1', 'admin@withergate.com', null, '$2a$10$D4OLKI6yy68crm.3imC9X.P2xqKHs5TloWUcr6z5XdOqnTrAK84ri', 'ADMIN', true);
+
+INSERT INTO oauth_client_details (client_id, resource_ids, client_secret, scope,
+authorized_grant_types, web_server_redirect_uri, authorities, access_token_validity,
+refresh_token_validity, additional_information, autoapprove) VALUES
+	('withergate', 'resource', '$2a$10$s7rR9qxaUnOJaE3J6ZSICupQxm.xTJhvScmXV.ylsI3AIY5OMXp.q', 'read,write', 'authorization_code,refresh_token,implicit,password,client_credentials', 'http://game.withergate.com', '', 3600, 2592000, '{}', 'true');
