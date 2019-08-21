bc1d5ac4a83edcd175377e5b0558c4faaac457d8
Martin Myslik <martin.myslik@ataccama.com>
2019-08-21 15:46:04 +0200

    task/#21 Update urls with https

--- src/main/resources/db/migration/V1_1__Init_Data.sql
+++ src/main/resources/db/migration/V1_1__Init_Data.sql
@@ -4,4 +4,4 @@ INSERT INTO users (user_id, email, pending_email, password, role, enabled) VALUE
 INSERT INTO oauth_client_details (client_id, resource_ids, client_secret, scope,
 authorized_grant_types, web_server_redirect_uri, authorities, access_token_validity,
 refresh_token_validity, additional_information, autoapprove) VALUES
-	('withergate', 'resource', '$2a$10$s7rR9qxaUnOJaE3J6ZSICupQxm.xTJhvScmXV.ylsI3AIY5OMXp.q', 'read,write', 'authorization_code,refresh_token,implicit,password,client_credentials', 'http://game.withergate.com', '', 3600, 2592000, '{}', 'true');
+	('withergate', 'resource', '$2a$10$s7rR9qxaUnOJaE3J6ZSICupQxm.xTJhvScmXV.ylsI3AIY5OMXp.q', 'read,write', 'authorization_code,refresh_token,implicit,password,client_credentials', 'https://game.withergate.com', '', 3600, 2592000, '{}', 'true');
